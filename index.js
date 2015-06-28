7f195645178f75406d0b0d5a5b96c4513e60b918
Willyham <willyd@uber.com>
2015-06-28 18:26:05 +0100

    Export union

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ var Bot = require('./bot');
 var TextTrigger = require('./triggers/text-trigger');
 var TrueTrigger = require('./triggers/true-trigger');
 var GroupTrigger = require('./triggers/group-trigger');
+var UnionTrigger = require('./triggers/union-trigger');
 
 module.exports = {
   Bot: Bot,
   Triggers: {
     TextTrigger: TextTrigger,
     GroupTrigger: GroupTrigger,
-    TrueTrigger: TrueTrigger
+    TrueTrigger: TrueTrigger,
+    UnionTrigger: UnionTrigger
   }
 };
